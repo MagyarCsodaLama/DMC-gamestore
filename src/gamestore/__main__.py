39478d8f51f4f3c8b59240b82a5fa0bf41129893
Bence <Hellopeter60@gmail.com>
2024-12-13 18:02:12 +0100

    #2 new files

---
+++ src/gamestore/__main__.py
@@ -0,0 +1,16 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+__version__ = '0.1.0'
+
+def main():
+    logger.info('Starting Gamestore %s', __version__)
+
+
+
+if __name__ == '__main__':
+    main()
